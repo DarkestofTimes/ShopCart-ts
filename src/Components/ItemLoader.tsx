f63703c770b9be89694e9b3ae3f1bbb20588e906
DarkestofTimes <133776070+DarkestofTimes@users.noreply.github.com>
2023-12-22 18:26:00 +0100

    Fix typing on ItemPage

--- src/Components/ItemLoader.tsx
+++ src/Components/ItemLoader.tsx
@@ -12,7 +12,7 @@ interface Request {
 export const ItemLoader: LoaderFunction = async ({
   params,
 }: LoaderFunctionArgs<Request>) => {
-  const item = JSON.parse(localStorage.getItem("item")) || null; //remove when finish with ItemPage
+  /* const item = JSON.parse(localStorage.getItem("item")) || null; //remove when finish with ItemPage
   const screens = JSON.parse(localStorage.getItem("screens")) || null;
   const additions = JSON.parse(localStorage.getItem("additions")) || null;
   const trailers = JSON.parse(localStorage.getItem("trailers")) || null;
@@ -24,21 +24,21 @@ export const ItemLoader: LoaderFunction = async ({
     trailers === null ||
     series === null
   ) {
-    //remove when finish with ItemPage
-    const [item, screens, additions, trailers, series] = await Promise.all([
-      fetchItem(params),
-      fetchScreens(params),
-      fetchAdditions(params),
-      fetchTrailers(params),
-      fetchSeries(params),
-    ]);
-    localStorage.setItem("item", JSON.stringify(item)); //remove when finish with ItemPage
+    //remove when finish with ItemPage */
+  const [item, screens, additions, trailers, series] = await Promise.all([
+    fetchItem(params),
+    fetchScreens(params),
+    fetchAdditions(params),
+    fetchTrailers(params),
+    fetchSeries(params),
+  ]);
+  /* localStorage.setItem("item", JSON.stringify(item)); //remove when finish with ItemPage
     localStorage.setItem("screens", JSON.stringify(screens));
     localStorage.setItem("additions", JSON.stringify(additions));
     localStorage.setItem("trailers", JSON.stringify(trailers));
     localStorage.setItem("series", JSON.stringify(series));
 
     return { item, screens, additions, trailers, series };
-  } //remove when finish with ItemPage
+  } //remove when finish with ItemPage */
   return { item, screens, additions, trailers, series };
 };
